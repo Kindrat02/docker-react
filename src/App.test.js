d226b519cb728304ab5a063b1c6188067c8a0476
Vitalii Kindrat <vkindrat@agileassets.com>
2023-01-22 00:07:31 +0200

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,18 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn Docker/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn Docker/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 3', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn Docker/i);
   expect(linkElement).toBeInTheDocument();
 });
